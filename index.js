81c4cf4917baba753ded860f5ee7860890c37b62
eustachekamala <eustachejared25@gmail.com>
2024-07-24 21:44:12 +0300

    The Big 0 assignment is done, I added in the function hasTargetSum that I devided the array half

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
